8c0836f26d03e84f7e24c3cde9b1ecb61f8bf482
fajarwahyu99 <fajarnugraha06@gmail.com>
2018-01-29 10:55:07 +0700

    Good Signifikan

--- app/src/main/java/com/example/helmyh/interfaceslide/Setorin.java
+++ app/src/main/java/com/example/helmyh/interfaceslide/Setorin.java
@@ -1,12 +1,28 @@
 package com.example.helmyh.interfaceslide;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import de.hdodenhof.circleimageview.CircleImageView;
 
-public class Setorin extends AppCompatActivity {
+public class Setorin extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
     TextView tvResultNama;
     String resultNama;
+    private ImageView imgview_barcode;
+    private CircleImageView imgview_fotoprofil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +36,113 @@ public class Setorin extends AppCompatActivity {
         if (extras != null)
             resultNama = extras.getString("result_nama");
         tvResultNama.setText(resultNama);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        initComponentsNavHeader();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
+    private void initComponentsNavHeader(){
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        View headerView = navigationView.getHeaderView(0);
 
-        private void initComponents(){
-            tvResultNama = (TextView) findViewById(R.id.tvResultNama);
+        imgview_barcode = (ImageView) headerView.findViewById(R.id.imgview_barcode);
+        imgview_barcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("barcode");
+            }
+        });
 
+        imgview_fotoprofil = (CircleImageView) headerView.findViewById(R.id.imgview_fotoprofil);
+        imgview_fotoprofil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Foto Profil");
+            }
+        });
+
+    }
+    private void initComponents() {
+        tvResultNama = (TextView) findViewById(R.id.tvResultNama);
+
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.setorin, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_saldo) {
+            showToast("Saldo");
+        } else if (id == R.id.nav_payment) {
+            showToast("Payment");
+        } else if (id == R.id.nav_setorin) {
+            showToast("Setorin");
+        } else if (id == R.id.nav_help) {
+            showToast("Help");
+        } else if (id == R.id.nav_share) {
+            showToast("Share Setorin ke teman");
+        } else if (id == R.id.nav_send) {
+            showToast("Kirim ke teman");
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    private void showToast(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
